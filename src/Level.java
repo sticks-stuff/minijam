13a228b607ba36b949798f77e8f7682e6f8167de
charlotte <charlottemackenzi@gmail.com>
2024-01-05 18:12:35 +1300

    draw level w/ temp vars

--- src/Level.java
+++ src/Level.java
@@ -2,15 +2,37 @@ import processing.core.*;
 
 public class Level {
 	private PApplet app;
-	private PImage GROUND;
-	private PImage BAD;
-	private PImage SLIP;
+	private static PImage ground;
+	private static PImage bad;
+	private static PImage slip;
+
+	private final int OFFSET_X = 1920;
+	private final int OFFSET_Y = 600;
+
 
 	public Level(PApplet applet) {
 		this.app = applet;
-		GROUND   = app.loadImage("../assets/ground.png");
-		BAD      = app.loadImage("../assets/bad.png");
-		SLIP     = app.loadImage("../assets/bad.png");
+		ground   = app.loadImage("../assets/ground.png");
+		bad      = app.loadImage("../assets/bad.png");
+		slip     = app.loadImage("../assets/bad.png");
+	}
+
+	public PImage getGround() {
+		return ground;
+	}
+	
+	public PImage getBad() {
+		return bad;
+	}
+
+	public PImage getSlip() {
+		return slip;
+	}
+
+	public void draw(int x, int y) {
+		app.image(ground, x - OFFSET_X, y - OFFSET_Y);
+		app.image(slip, x - OFFSET_X, y - OFFSET_Y);
+		app.image(bad, x - OFFSET_X, y - OFFSET_Y);
 	}
 
 }
