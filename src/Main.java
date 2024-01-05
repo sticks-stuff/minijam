9828e5253c431b04839d43bf531638f47a92e067
Oliver Geenty <oligeenty@gmail.com>
2024-01-05 17:20:20 +1300

    Add processing

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public void settings(){
+        size(600,600);
+    }
+
+    public void setup(){}
+    public void draw(){}
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
 }
