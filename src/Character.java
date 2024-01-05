e051a56e75ac3e77a47b00f128a2ca7f4562e42f
Oliver Geenty <oligeenty@gmail.com>
2024-01-05 18:00:25 +1300

    Update character with rope

--- src/Character.java
+++ src/Character.java
@@ -3,13 +3,36 @@ public class Character {
     private PApplet app;
     private float x;
     private float y;
+
+    private boolean attached = false;
+    private float attachedX;
+    private float attachedY;
+
     public Character(PApplet app,int x, int y){
         this.app = app;
         this.x = x;
         this.y = y;
     }
 
+    public boolean attachTo(float x, float y){
+        attached = true;
+        attachedX = x;
+        attachedY = y;
+        return attached;
+    }
+
+    public void detach(){
+        this.attached = false;
+    }
+
     public void draw(){
         app.rect(x,y,10,20);
+        if (attached){
+            float yAttachmentOffset = 0;
+            float xAttachmentOffset = 5;
+            app.line(x+ xAttachmentOffset,y+ yAttachmentOffset,attachedX,attachedY);
+        }
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 import processing.core.PApplet;
 
 public class Main extends PApplet {
+
+    Character c;
     public void settings(){
         size(200,200);
     }
-    Character c;
 
     public void setup(){
         c = new Character(this,100,100);
     }
     public void draw(){
+        background(255,255,255);
         c.draw();
+    }
+
+    public void mousePressed(){
+        c.attachTo(mouseX,mouseY);
+    }
+
+    public void mouseReleased(){
+        c.detach();
     }
 
     public static void main(String[] args) {
