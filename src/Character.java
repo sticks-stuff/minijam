2473a0b462c4fcebe7785efde64ff3f121f3d2ab
Oliver Geenty <oligeenty@gmail.com>
2024-01-05 21:38:35 +1300

    Remove print

--- src/Character.java
+++ src/Character.java
@@ -65,7 +65,7 @@ public class Character {
             return;
         }
         app.image(charImage, (float) app.width/2,(float) app.height/2);
-        System.out.println(deltaTime);
+        
 
 
 
